94d6c9ec3b75ebbe4b55e18dd929d1cec30be498
Kauan <Kauan@DESKTOP-3G0BSM7>
2025-02-10 23:19:46 -0300

    Organização de código MySQLConnection

--- src/main/java/com/javaswing/dao/MySQLConnection.java
+++ src/main/java/com/javaswing/dao/MySQLConnection.java
@@ -18,8 +18,8 @@ public class MySQLConnection {
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
-public static void main(String[] args) {
-    try {
+    public static void main(String[] args) {
+        try {
                 // Carregando o driver JDBC
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 System.out.println("Driver carregado com sucesso.");
@@ -27,23 +27,23 @@ public static void main(String[] args) {
                 // Estabelecendo a conexão
                 Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
             if (connection != null) {
-                System.out.println("Conexão estabelecida com sucesso!");
+                    System.out.println("Conexão estabelecida com sucesso!");
 
-                // Faça aqui as operações no banco de dados, se necessário
+                    // Faça aqui as operações no banco de dados, se necessário
 
-                // Fechando a conexão
+                    // Fechando a conexão
                 connection.close();
-                System.out.println("Conexão fechada com sucesso.");
+                    System.out.println("Conexão fechada com sucesso.");
                 
             } else {
-                System.out.println("Falha ao estabelecer conexão.");
+                   System.out.println("Falha ao estabelecer conexão.");
             }
             
-       } catch (ClassNotFoundException e) {
-            System.out.println("Driver JDBC não encontrado: " + e.getMessage());
+            } catch (ClassNotFoundException e) {
+                   System.out.println("Driver JDBC não encontrado: " + e.getMessage());
             
-       } catch (SQLException e) {
-            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            } catch (SQLException e) {
+                   System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
         }
     }
 }
