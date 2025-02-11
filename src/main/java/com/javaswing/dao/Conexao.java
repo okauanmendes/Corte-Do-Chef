2957d8e1dae5f62b8e3e445ff02920c0416f2adb
Kauan <Kauan@DESKTOP-3G0BSM7>
2025-02-10 23:02:37 -0300

    Organização de código no Conexão.java

--- src/main/java/com/javaswing/dao/Conexao.java
+++ src/main/java/com/javaswing/dao/Conexao.java
@@ -9,13 +9,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 /**
  *
- * @author Jorge Lescura
+ * @author Kauan Mendes
  */
 public class Conexao {
     
-    private static final String URL = "jdbc:mysql://localhost:3306/acougue";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+private static final String URL = "jdbc:mysql://localhost:3306/acougue";
+private static final String USER = "root";
+private static final String PASSWORD = "";
     
     
 //    public static void main(String[] args) {
@@ -26,31 +26,31 @@ public class Conexao {
 
     
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
+public static Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(URL, USER, PASSWORD);
+}
 
     
-  public Connection conectar() {
+public Connection conectar() {
     Connection connection = null;
-    try {
-      connection = Conexao.getConnection();
-      System.out.println("Conectou no banco de dados.");
-    } catch (SQLException ex) {
-      System.out.println("Erro: Não conseguiu conectar no BD.");
-    } 
+        try {
+            connection = Conexao.getConnection();
+            System.out.println("Conectou no banco de dados.");
+        } catch (SQLException ex) {
+            System.out.println("Erro: Não conseguiu conectar no BD.");
+        } 
 
-    return connection;
-  }
+        return connection;
+}
 
-  public void desconectar(Connection conn) {
+public void desconectar(Connection conn) {
     try {
-      if(conn != null && !conn.isClosed()) {
-        conn.close();
-        System.out.println("Desconectou do banco de dados.");
-      }
+            if(conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Desconectou do banco de dados.");
+            }
     } catch (SQLException ex) {
-      System.out.println("Não conseguiu desconectar do BD.");
-    }
+            System.out.println("Não conseguiu desconectar do BD.");
+            }
   }
 }
