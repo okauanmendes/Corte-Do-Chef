8b19491a746b1dcd589ee83e9e4ae22cd36d918e
Kauan <Kauan@DESKTOP-3G0BSM7>
2025-02-10 23:27:07 -0300

    Organização de código VendaDao

--- src/main/java/com/javaswing/dao/VendaDao.java
+++ src/main/java/com/javaswing/dao/VendaDao.java
@@ -11,27 +11,29 @@ import java.sql.Statement;
 
 /**
  *
- * @author LAB 03
+ * @author Kauan Mendes
  */
+
 public class VendaDao {
     public boolean vender(Venda venda) throws SQLException{
         Conexao conexao = new Conexao();
         Connection connection = conexao.conectar();
         
-        String query = "INSERT INTO Venda (Mercadoria,Kilo,Quantidade,preco) values("+
-               "'" + venda.getMercadoria() + "'," +
+            String query = "INSERT INTO Venda (Mercadoria,Kilo,Quantidade,preco) values("+
+                "'" + venda.getMercadoria() + "'," +
                      venda.getKilo()   + "," +
                      venda.getQuantidade()  + "," +
                      venda.getPreco() + ")";
-        try{
-                Statement statement = connection.createStatement();
-                statement.execute(query);
-                conexao.desconectar(connection);
-                return true;
-        }catch (SQLException ex) {
-            System.out.print(ex.initCause(ex));
-            return false;
-        }    
+            try{
+                    Statement statement = connection.createStatement();
+                    statement.execute(query);
+                    conexao.desconectar(connection);
+                    return true;
+                    
+                }catch (SQLException ex) {
+                        System.out.print(ex.initCause(ex));
+                        return false;
+                }    
         
     }
      
